cde57082b76e7ab0873911b40e9121488f22b9aa
fitrh <fitrahm17h@student.unhas.ac.id>
2021-04-02 15:58:22 +0800

    apply boxdraw patch

--- st.h
+++ st.h
@@ -33,6 +33,7 @@ enum glyph_attribute {
 	ATTR_WRAP       = 1 << 8,
 	ATTR_WIDE       = 1 << 9,
 	ATTR_WDUMMY     = 1 << 10,
+	ATTR_BOXDRAW    = 1 << 11,
 	ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
 };
 
@@ -81,6 +82,18 @@ void die(const char *, ...);
 void redraw(void);
 void draw(void);
 
+int isboxdraw(Rune);
+ushort boxdrawindex(const Glyph *);
+#ifdef XFT_VERSION
+/* only exposed to x.c, otherwise we'll need Xft.h for the types */
+void boxdraw_xinit(Display *, Colormap, XftDraw *, Visual *);
+void drawboxes(
+        int, int, int, int,
+        XftColor *, XftColor *, const XftGlyphFontSpec *, int
+);
+#endif
+
+
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
@@ -112,6 +125,7 @@ void *xrealloc(void *, size_t);
 char *xstrdup(char *);
 
 /* config.h globals */
+extern const int boxdraw, boxdraw_bold, boxdraw_braille;
 extern char *utmp;
 extern char *scroll;
 extern char *stty_args;
