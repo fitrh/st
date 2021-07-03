fcaebe04a92e21b9ca902b79c7e3b618c6f59089
fitrh <fitrahm17h@student.unhas.ac.id>
2021-07-03 17:51:02 +0800

    [ update config ] remove utmp program

--- config.def.h
+++ config.def.h
@@ -30,7 +30,7 @@ static int borderperc = 100;
  * 5: value of shell in config.h
  */
 static char *shell = "/usr/local/bin/fish";
-char *utmp = "/usr/local/bin/utmp";
+char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
 char *scroll = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
