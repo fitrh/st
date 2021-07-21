d87e04e22749c62b6bec3821c34c32546d852238
fitrh <fitrahm17h@student.unhas.ac.id>
2021-07-22 00:16:52 +0800

    [ upstream ] Mild const-correctness improvements
    commit 4536f46[1] from upstream
    
    [1]: https://git.suckless.org/st/commit/4536f46cfff50c66a115755def0155d8e246b02f.html

--- x.c
+++ x.c
@@ -161,7 +161,7 @@ static void xresize(int, int);
 static void xhints(void);
 static int xloadcolor(int, const char *, Color *);
 static int xloadfont(Font *, FcPattern *);
-static void xloadfonts(char *, double);
+static void xloadfonts(const char *, double);
 static int xloadsparefont(FcPattern *, int);
 static void xloadsparefonts(void);
 static void xunloadfont(Font *);
@@ -963,7 +963,7 @@ xloadfont(Font *f, FcPattern *pattern)
 }
 
 void
-xloadfonts(char *fontstr, double fontsize)
+xloadfonts(const char *fontstr, double fontsize)
 {
 	FcPattern *pattern;
 	double fontval;
@@ -971,7 +971,7 @@ xloadfonts(char *fontstr, double fontsize)
 	if (fontstr[0] == '-')
 		pattern = XftXlfdParse(fontstr, False, False);
 	else
-		pattern = FcNameParse((FcChar8 *)fontstr);
+		pattern = FcNameParse((const FcChar8 *)fontstr);
 
 	if (!pattern)
 		die("can't open font %s\n", fontstr);
